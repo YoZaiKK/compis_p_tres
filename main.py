cbb1c9e8b74e1d9a6f628cdcd79af461ff53256c
Guillermo <memocargo@hotmail.com>
2021-10-13 14:38:09 -0500

    Lectura de archivo txt

---
+++ main.py
@@ -0,0 +1,67 @@
+# leer arcchivo txt
+entrada = input("Introduce el nombre del archivo a leer en la carpeta, sin espasios y con la extencion (Ejemplo: 1.txt): ")
+f = open(entrada, 'r')
+
+EstadosQ = []
+Epsilon = []
+EdoInicial = ''
+EdosAceptacion = []
+Sigma = []
+
+# Metemos los estados a nuestra lista de esados
+mensaje = f.readline()
+for caracter in mensaje:
+    if caracter != ',' and caracter != '\n':
+        EstadosQ.append(caracter)
+
+# Metemos los simbolos a nuestra lista de Epsilon
+mensaje = f.readline()
+for caracter in mensaje:
+    if caracter != ',' and caracter != '\n':
+        Epsilon.append(caracter)
+
+# Metemos el estado de start a nuestra lista de Epsilon
+mensaje = f.readline()
+for caracter in mensaje:
+    if caracter != ',' and caracter != '\n':
+        EdoInicial = caracter
+
+# Metemos los estados finales a nuestra lista de estadosFinales
+mensaje = f.readline()
+x = ''
+for caracter in mensaje:
+    if caracter != ',' and caracter != '\n':
+        x = x+caracter
+    else:
+        EdosAceptacion.append(x)
+        x = ''
+if x != '':
+    EdosAceptacion.append(x)
+
+# Sigue sigma UTF-8 = "\u03A3"
+
+mensaje = f.readline() #Leemos linea del archivo txt
+while(mensaje): # Si el resultado de intentar leer es exitoso, analizamos la linea de transicion 
+    cadena_aux = '' #Si los estados/transiciones son de mas de un caracter aqui se acumularan 
+    transicion = [] #Se adjuntará una lista con los datos de la transicion a la lista de transiciones Sigma
+    x = 0 #Para leer cada transicion
+    while x < len(mensaje): #Leemos cada transicion caracter por caracter
+        if mensaje[x] != '\n' and mensaje[x] != ',': #Si el caracter es diferente que una coma o un salto de linea...
+            cadena_aux = cadena_aux + mensaje[x] #... el caracter se añade a la cadena formada por los caracteres 
+        elif mensaje[x] == '\n' or mensaje[x] == ',' or x == (len(mensaje) - 1): #Si es un salto de linea, coma o corchete de cierre...
+            transicion.append(cadena_aux) #Se adjunta a la lista con los datos
+            cadena_aux = '' # Se reinicia la cadena auxiliar 
+        x +=  1  #Iteramos caracter
+    if len(transicion) < 3: # Si el largo de la lista con los datos significa que nos falta uno; El ultimo
+        transicion.append(cadena_aux) #Agregamos el ultimo dato
+    Sigma.append(transicion) #Agregamos la transicion a la lista de transiciones Sigma
+    mensaje = f.readline() #Leemos siguiente linea
+
+f.close() # Cerramos el archivo
+
+#print(f'\u03A3: {Sigma}') #Mostramos transiciones 
+#print(f'EdosAceptacion: {EdosAceptacion}') #Mostramos transiciones 
+#print(f'EdoInicial: {EdoInicial}') #Mostramos transiciones 
+
+#Estado_final = funcion(Sigma, input("Introduzca cadena: "), Epsilon, EdoInicial, EdosAceptacion) #Pedimos la cadena y Comenzamos el analisis de la cadena con el automata 
+#print(f'Estados finales:\n{Estado_final}') #Mostramos estados finales 
